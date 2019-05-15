c0c66ab8e492b2f515d20bd2f4e72aad13edb5d9
hongtun <hongtun@ahyzyx.cn>
2019-05-15 19:02:35 +0800

    login

--- pages/login.vue
+++ pages/login.vue
@@ -1,37 +1,57 @@
 <template>
-  <div class="container">
-    <h1>Please login to see the secret content</h1>
-    <form v-if="!$store.state.authUser" @submit.prevent="login">
-      <p v-if="formError" class="error">
-        {{ formError }}
-      </p>
-      <p><i>To login, use <b>demo</b> as username and <b>demo</b> as password.</i></p>
-      <p>Username: <input v-model="formUsername" type="text" name="username"></p>
-      <p>Password: <input v-model="formPassword" type="password" name="password"></p>
-      <button type="submit">
-        Login
-      </button>
-    </form>
-    <div v-else>
-      Hello {{ $store.state.authUser.username }}!
-      <pre>I am the secret content, I am shown only when the user is connected.</pre>
-      <p><i>You can also refresh this page, you'll still be connected!</i></p>
-      <button @click="logout">
-        Logout
-      </button>
-    </div>
-    <p>
-      <NuxtLink to="/secret">
-        Super secret page
-      </NuxtLink>
-    </p>
-  </div>
+  <el-container>
+    <el-main>
+      <el-row type="flex">
+        <el-col :span="24">
+          <div class="grid-content">
+            <h3 class="title">Please login to see the index content</h3>
+          </div>
+          </el-col>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <el-col :span="12" class="input-box">
+          <el-form v-if="!$store.state.authUser" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+            <el-form-item label="用户名">
+              <el-input v-model="formUsername"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-model="formPassword"></el-input>
+            </el-form-item>
+            <el-button type="primary" @click="login">登录</el-button>
+          </el-form>
+          <div v-else>
+            Hello {{ $store.state.authUser.username }}!
+            <pre>I am the secret content, I am shown only when the user is connected.</pre>
+            <p><i>You can also refresh this page, you'll still be connected!</i></p>
+            <button @click="logout">
+              Logout
+            </button>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center" class="input-box">
+        <el-col :span="12">
+          <p class="link">
+            <NuxtLink to="/">
+              Super index page
+            </NuxtLink>
+          </p>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      },
       formError: null,
       formUsername: '',
       formPassword: ''
@@ -48,6 +68,7 @@ export default {
         this.formPassword = ''
         this.formError = null
       } catch (e) {
+        this.$message.error(e.message);
         this.formError = e.message
       }
     },
@@ -62,11 +83,23 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  padding: 100px;
+<style lang="less" scoped>
+.grid-content{
+  padding-top: 100px;
+  text-align: center;
+  .title{
+    font-size:46px; 
+    font-weight: bolder;
+    margin: 40px 0 40px;
+  }
+}
+.input-box{
+  text-align: center;
 }
 .error {
   color: red;
+}
+.link{
+  margin-top: 45px;
 }
 </style>
